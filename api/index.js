23c8431c7921e51f04451aabc0133d6b2a4a24a5
anwarkvzk <anvarvzk521@gmail.com>
2022-10-11 19:57:22 +0530

    JWT Role-based Authorization and verification

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/auth.js";
 import hotelsRoute from "./routes/hotels.js";
 import usersRoute from "./routes/users.js";
 import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -24,6 +25,7 @@ mongoose.connection.on("disconnected", () => {
 });
 
 //midleware
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
