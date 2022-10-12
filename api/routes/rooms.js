f3bdced33f6c9264294004286d60a22272ed831d
anwarkvzk <anvarvzk521@gmail.com>
2022-10-12 13:47:40 +0530

    MongoDB Model Relations

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,8 +1,22 @@
 import express, { Router } from "express";
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../controllers/room.js";
+import {verifyAdmin} from '../utils/verifyToken.js'
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello, this is hotels endpoint");
-});
+
+//CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+//UPDATE
+router.put("/:id", verifyAdmin, updateRoom);
+
+//DELETE
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+
+//GET
+router.get("/:id", verifyAdmin, getRoom);
+
+//GET ALL
+router.get("/", getRooms);
 
 export default router;
